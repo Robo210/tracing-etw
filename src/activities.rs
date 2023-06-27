3e889f45b7fd46f99975a1884bf924bd074699fd
Kyle Sabo <robo210@gmail.com>
2023-06-27 02:51:58 -0700

    don't need string versions of span id

--- src/activities.rs
+++ src/activities.rs
@@ -1,11 +1,6 @@
-use std::io::Write;
-use std::{io::Cursor, mem::MaybeUninit};
-
 pub(crate) struct Activities {
-    pub(crate) span_id: [u8; 16],                    // Hex string
     pub(crate) activity_id: [u8; 16],                // Guid
     pub(crate) parent_activity_id: Option<[u8; 16]>, // Guid
-    pub(crate) parent_span_id: [u8; 16],             // Hex string
 }
 
 impl Default for Activities {
@@ -21,29 +16,19 @@ impl Activities {
         let (_, half) = activity_id.split_at_mut(8);
         half.copy_from_slice(&span_id.to_le_bytes());
 
-        let (parent_activity_id, parent_span_name) = if parent_span_id == 0 {
-            (None, [0; 16])
+        let parent_activity_id = if parent_span_id == 0 {
+            None
         } else {
-            let mut buf: [u8; 16] = unsafe { MaybeUninit::uninit().assume_init() };
-            let mut cur = Cursor::new(&mut buf[..]);
-            write!(&mut cur, "{:16x}", span_id).expect("!write");
-
             let mut activity_id: [u8; 16] = [0; 16];
             let (_, half) = activity_id.split_at_mut(8);
             half.copy_from_slice(&parent_span_id.to_le_bytes());
 
-            (Some(activity_id), buf)
+            Some(activity_id)
         };
 
-        let mut buf: [u8; 16] = unsafe { MaybeUninit::uninit().assume_init() };
-        let mut cur = Cursor::new(&mut buf[..]);
-        write!(&mut cur, "{:16x}", span_id).expect("!write");
-
         Activities {
-            span_id: buf,
             activity_id,
             parent_activity_id,
-            parent_span_id: parent_span_name,
         }
     }
 }
