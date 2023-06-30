445e322ea89a7ae8bef5e6e3aed5919124b46a6c
Kyle Sabo <robo210@gmail.com>
2023-06-30 04:03:34 -0700

    Update test app to write an event outside of a span

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -11,13 +11,6 @@ fn main() {
         .with(tracing_subscriber::fmt::layer().with_span_events(FmtSpan::ACTIVE));
     let _sub = subscriber.try_init();
 
-    // Construct a new span named "my span" with trace log level.
-    let span = span!(Level::INFO, "my_span", field3 = 4.5, field2 = 0, field1 = 0);
-
-    // Enter the span, returning a guard object.
-    let _enter = span.enter();
-    span.record("field1", 12345);
-
     #[allow(non_snake_case)]
     let fieldB = "asdf";
     event!(
@@ -28,6 +21,13 @@ fn main() {
         "inside {}!",
         "main"
     );
+
+    // Construct a new span named "my span" with trace log level.
+    let span = span!(Level::INFO, "my_span", field3 = 4.5, field2 = 0, field1 = 0);
+
+    // Enter the span, returning a guard object.
+    let _enter = span.enter();
+    span.record("field1", 12345);
 
     span.record("field2", "9876f64");
 
